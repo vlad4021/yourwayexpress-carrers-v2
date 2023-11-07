d925f9a3d85f7caf4cd54e9a61712e05a309eb0a
vlad4021 <149450033+vlad4021@users.noreply.github.com>
2023-11-07 01:35:44 +0000

    Added everything
    added

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+    {
+        'id':1,
+        'title':'Data Analyst',
+        'location': 'Columbus,OH',
+        'salary': '$100,000,00',
+    },
+    {
+        'id':2,
+        'title':'Hacker',
+        'location': 'Seattle,WA',
+        'salary': '$300,000,00',
+    }
+]
+
+
+@app.route("/")
+def hello_jovian():
+    return render_template('home.html',
+                           jobs=JOBS,
+                           Company_Name='Jovian')
+
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
